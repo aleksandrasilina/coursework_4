7e698199affdc19c0b9b25afa38e9997a9a9b3e8
Александра <aleksandrasilina@gmail.com>
2024-05-15 11:24:58 +0300

    add abstract class Parser

---
+++ src/parser.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class Parser(ABC):
+    """Абстрактный класс для работы с API сервиса с вакансиями"""
+
+    def __init__(self, file_worker: str):
+        self.file_worker = file_worker
+
+    @abstractmethod
+    def load_vacancies(self, keyword: str):
+        """Метод для загрузки списка вакансий с API сервиса с вакансиями"""
+        pass
