c1dc7b800e7385ed475c07aad57e69b92df7d153
Александра <aleksandrasilina@gmail.com>
2024-05-17 23:12:08 +0300

    add abspath to file

--- src/json_saver.py
+++ src/json_saver.py
@@ -15,13 +15,15 @@ class JSONSaver(VacancySaver):
 
     def add_vacancies(self, vacancies: list[Vacancy]):
         """Метод для сохранения списка экземпляров вакансий в файл"""
-        with open(os.path.join('..', 'data', self.file_name), 'w', encoding='utf-8') as file:
+        with open(os.path.join('C:/Users/Александра/PycharmProjects/coursework_4/data', self.file_name), 'w',
+                  encoding='utf-8') as file:
             vacancies_list = [vac.__dict__() for vac in vacancies]
             json.dump(vacancies_list, file, ensure_ascii=False, indent=4)
 
     def get_vacancies(self) -> list[dict]:
         """Возвращает содержимое файла"""
-        with open(os.path.join('..', 'data', self.file_name), 'r', encoding='utf-8') as file:
+        with open(os.path.join('C:/Users/Александра/PycharmProjects/coursework_4/data', self.file_name), 'r',
+                  encoding='utf-8') as file:
             return json.load(file)
 
     def filter_vacancies(self, keywords: list[str]) -> list[Vacancy]:
@@ -42,7 +44,8 @@ class JSONSaver(VacancySaver):
         """Метод для добавления вакансии в файл"""
         vacancies_data = self.get_vacancies()
         vacancies_data.append(vacancy.__dict__())
-        with open(os.path.join('..', 'data', self.file_name), 'w', encoding='utf-8') as file:
+        with open(os.path.join('C:/Users/Александра/PycharmProjects/coursework_4/data', self.file_name), 'w',
+                  encoding='utf-8') as file:
             json.dump(vacancies_data, file, ensure_ascii=False, indent=4)
 
     def delete_vacancy(self, vacancy: Vacancy):
@@ -51,5 +54,6 @@ class JSONSaver(VacancySaver):
         for vac in vacancies_data:
             if vac['id'] == vacancy.id:
                 vacancies_data.remove(vac)
-        with open(os.path.join('..', 'data', self.file_name), 'w', encoding='utf-8') as file:
+        with open(os.path.join('C:/Users/Александра/PycharmProjects/coursework_4/data', self.file_name), 'w',
+                  encoding='utf-8') as file:
             json.dump(vacancies_data, file, ensure_ascii=False, indent=4)
