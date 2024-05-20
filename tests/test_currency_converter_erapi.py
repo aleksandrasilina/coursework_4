d05a74b7911209f576d17eb02dd20413c4e706e2
Александра <aleksandrasilina@gmail.com>
2024-05-20 11:41:20 +0300

    fix test_currency_converter_erapi.py: add tests with requests-mock

--- tests/test_currency_converter_erapi.py
+++ tests/test_currency_converter_erapi.py
@@ -6,11 +6,16 @@ def test_currency_converter_erapi_init(cc_api):
     assert cc_api.url == f"https://open.er-api.com/v6/latest/CNY"
 
 
-def test_currency_converter_erapi_get_all_exchange_rates_erapi(cc_api):
+def test_currency_converter_erapi_get_all_exchange_rates_erapi(requests_mock, cc_api):
     """Тестирует метод для запроса ExchangeRate API и преобразования его в словарь с курсами валют"""
-    assert len(cc_api.get_all_exchange_rates_erapi()) == 162
+    data = {'rates': {'RUB': 12.83}, 'result': 'success'}
+    requests_mock.register_uri('GET', f'https://open.er-api.com/v6/latest/{cc_api.src}', json=data)
+    ruble_rate = cc_api.get_all_exchange_rates_erapi()
+    assert ruble_rate.get('RUB') == 12.83
 
 
-def test_currency_converter_erapi_convert_currency_erapi(cc_api):
+def test_currency_converter_erapi_convert_currency_erapi(requests_mock, cc_api):
     """Тестирует метод для конвертации валюты в рубли"""
-    assert cc_api.convert_currency_erapi() == 12582
+    data = {'rates': {'RUB': 12.83}, 'result': 'success'}
+    requests_mock.register_uri('GET', f'https://open.er-api.com/v6/latest/{cc_api.src}', json=data)
+    assert cc_api.convert_currency_erapi() == 12830
