710c6f57d49cf4dfebd3e5d72f5937a6360899bb
Александра <aleksandrasilina@gmail.com>
2024-05-20 23:38:38 +0300

    add private attributes

--- src/head_hunter_api.py
+++ src/head_hunter_api.py
@@ -7,17 +7,47 @@ class HeadHunterAPI(Parser):
 
     def __init__(self, file_worker: str):
         """Конструктор для API HeadHunter"""
-        self.url = 'https://api.hh.ru/vacancies'
-        self.headers = {'User-Agent': 'HH-User-Agent'}
-        self.params = {'text': '', 'page': 0, 'per_page': 100}
-        self.vacancies = []
+        self.__url = 'https://api.hh.ru/vacancies'
+        self.__headers = {'User-Agent': 'HH-User-Agent'}
+        self.__params = {'text': '', 'page': 0, 'per_page': 100}
+        self.__vacancies = []
         super().__init__(file_worker)
 
     def load_vacancies(self, keyword: str):
         """Метод для загрузки списка вакансий с API HeadHunter"""
-        self.params['text'] = keyword
-        while self.params.get('page') != 20:
-            response = requests.get(self.url, headers=self.headers, params=self.params)
+        self.__params['text'] = keyword
+        while self.__params.get('page') != 20:
+            response = requests.get(self.__url, headers=self.__headers, params=self.__params)
             vacancies = response.json()['items']
-            self.vacancies.extend(vacancies)
-            self.params['page'] += 1
+            self.__vacancies.extend(vacancies)
+            self.__params['page'] += 1
+
+    @property
+    def url(self):
+        """Геттер для url"""
+        return self.__url
+
+    @property
+    def headers(self):
+        """Геттер для headers"""
+        return self.__headers
+
+    @property
+    def params(self):
+        """Геттер для params"""
+        return self.__params
+
+    @property
+    def vacancies(self):
+        """Геттер для списка вакансий"""
+        return self.__vacancies
+
+    @property
+    def file_worker(self):
+        """Геттер для имени файла"""
+        return self._file_worker
+
+    @file_worker.setter
+    def file_worker(self, file_worker):
+        """Геттер для имени файла"""
+        self._file_worker = file_worker
